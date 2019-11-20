f4387f9af7d6e5683eaf03607105ded1cfa07df6
Daniel Sparber <daniel@sparber.io>
2019-11-19 21:35:09 -0500

    Multithreading

---
+++ assignment_package/src/scene/createchunkrunnable.cpp
@@ -0,0 +1,6 @@
+#include "createchunkrunnable.h"
+
+CreateChunkRunnable::CreateChunkRunnable()
+{
+
+}
---
+++ assignment_package/src/scene/createchunkrunnable.h
@@ -0,0 +1,11 @@
+#ifndef CREATECHUNKRUNNABLE_H
+#define CREATECHUNKRUNNABLE_H
+
+
+class CreateChunkRunnable
+{
+public:
+    CreateChunkRunnable();
+};
+
+#endif // CREATECHUNKRUNNABLE_H
